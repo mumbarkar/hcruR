ce1f86340eb04fae7f25329c28e56e329b8ca705
mumbarkar <maheshubr30@gmail.com>
2025-05-10 07:08:36 +0530

    https://cran.r-project.org/bin/windows/Rtools/

--- R/compare_hcru_cohorts.R
+++ R/compare_hcru_cohorts.R
@@ -8,8 +8,8 @@
 compare_hcru_cohorts <- function(hcru_summary, cohort) {
   hcru_summary <- dplyr::left_join(hcru_summary, cohort, by = "person_id")
 
-  hcru_summary %>%
-    dplyr::group_by(cohort_id, domain, time_window) %>%
+  hcru_summary |>
+    dplyr::group_by(cohort_id, domain, time_window) |>
     dplyr::summarise(
       mean_count = mean(event_count, na.rm = TRUE),
       median_count = median(event_count, na.rm = TRUE),
--- R/estimate_hcru.R
+++ R/estimate_hcru.R
@@ -8,7 +8,7 @@
 #' @return Dataframe with counts and costs per patient per domain and time window
 #' @export
 estimate_hcru <- function(cohort, hcru, pre_days = 180, post_days = 365) {
-  hcru <- dplyr::left_join(hcru, cohort, by = "person_id") %>%
+  hcru <- dplyr::left_join(hcru, cohort, by = "person_id") |>
     dplyr::mutate(
       relative_day = as.integer(event_date - index_date),
       time_window = dplyr::case_when(
@@ -16,11 +16,11 @@ estimate_hcru <- function(cohort, hcru, pre_days = 180, post_days = 365) {
         relative_day >= 0 & relative_day <= post_days ~ "post",
         TRUE ~ NA_character_
       )
-    ) %>%
+    ) |>
     dplyr::filter(!is.na(time_window))
 
-  summary <- hcru %>%
-    dplyr::group_by(person_id, domain, time_window) %>%
+  summary <- hcru |>
+    dplyr::group_by(person_id, domain, time_window) |>
     dplyr::summarise(
       event_count = dplyr::n(),
       total_cost = sum(cost, na.rm = TRUE),
